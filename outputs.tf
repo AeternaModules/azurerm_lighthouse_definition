a8f4feb9c496c34d0f98f56f3ee7a05a61336e92
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "lighthouse_definitions" {
-  description = "All lighthouse_definition resources"
-  value       = azurerm_lighthouse_definition.lighthouse_definitions
-}
 output "lighthouse_definitions_authorization" {
-  description = "List of authorization values across all lighthouse_definitions"
-  value       = [for k, v in azurerm_lighthouse_definition.lighthouse_definitions : v.authorization]
+  description = "Map of authorization values across all lighthouse_definitions, keyed the same as var.lighthouse_definitions"
+  value       = { for k, v in azurerm_lighthouse_definition.lighthouse_definitions : k => v.authorization }
 }
 output "lighthouse_definitions_description" {
-  description = "List of description values across all lighthouse_definitions"
-  value       = [for k, v in azurerm_lighthouse_definition.lighthouse_definitions : v.description]
+  description = "Map of description values across all lighthouse_definitions, keyed the same as var.lighthouse_definitions"
+  value       = { for k, v in azurerm_lighthouse_definition.lighthouse_definitions : k => v.description }
 }
 output "lighthouse_definitions_eligible_authorization" {
-  description = "List of eligible_authorization values across all lighthouse_definitions"
-  value       = [for k, v in azurerm_lighthouse_definition.lighthouse_definitions : v.eligible_authorization]
+  description = "Map of eligible_authorization values across all lighthouse_definitions, keyed the same as var.lighthouse_definitions"
+  value       = { for k, v in azurerm_lighthouse_definition.lighthouse_definitions : k => v.eligible_authorization }
 }
 output "lighthouse_definitions_lighthouse_definition_id" {
-  description = "List of lighthouse_definition_id values across all lighthouse_definitions"
-  value       = [for k, v in azurerm_lighthouse_definition.lighthouse_definitions : v.lighthouse_definition_id]
+  description = "Map of lighthouse_definition_id values across all lighthouse_definitions, keyed the same as var.lighthouse_definitions"
+  value       = { for k, v in azurerm_lighthouse_definition.lighthouse_definitions : k => v.lighthouse_definition_id }
 }
 output "lighthouse_definitions_managing_tenant_id" {
-  description = "List of managing_tenant_id values across all lighthouse_definitions"
-  value       = [for k, v in azurerm_lighthouse_definition.lighthouse_definitions : v.managing_tenant_id]
+  description = "Map of managing_tenant_id values across all lighthouse_definitions, keyed the same as var.lighthouse_definitions"
+  value       = { for k, v in azurerm_lighthouse_definition.lighthouse_definitions : k => v.managing_tenant_id }
 }
 output "lighthouse_definitions_name" {
-  description = "List of name values across all lighthouse_definitions"
-  value       = [for k, v in azurerm_lighthouse_definition.lighthouse_definitions : v.name]
+  description = "Map of name values across all lighthouse_definitions, keyed the same as var.lighthouse_definitions"
+  value       = { for k, v in azurerm_lighthouse_definition.lighthouse_definitions : k => v.name }
 }
 output "lighthouse_definitions_plan" {
-  description = "List of plan values across all lighthouse_definitions"
-  value       = [for k, v in azurerm_lighthouse_definition.lighthouse_definitions : v.plan]
+  description = "Map of plan values across all lighthouse_definitions, keyed the same as var.lighthouse_definitions"
+  value       = { for k, v in azurerm_lighthouse_definition.lighthouse_definitions : k => v.plan }
 }
 output "lighthouse_definitions_scope" {
-  description = "List of scope values across all lighthouse_definitions"
-  value       = [for k, v in azurerm_lighthouse_definition.lighthouse_definitions : v.scope]
+  description = "Map of scope values across all lighthouse_definitions, keyed the same as var.lighthouse_definitions"
+  value       = { for k, v in azurerm_lighthouse_definition.lighthouse_definitions : k => v.scope }
 }
 
